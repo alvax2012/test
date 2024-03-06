7cc8b1fd41464b2ad3fec8c7e2f3ad5b00844009
alvax2012 <39165799+alvax2012@users.noreply.github.com>
2024-03-06 16:18:36 +0800

    Update index.py
    1

--- index.py
+++ index.py
@@ -1,14 +1,2 @@
-print("hello!")
-
-
-rangelist = range(10)
-[0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-
-for number in rangelist:
-    
-if number in (3, 4, 7, 9):
-    break
-else:
-    continue
-else:
-pass
+word = 'Hello, World'
+print(word) # Hello, World
