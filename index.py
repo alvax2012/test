bb5735db22813e55b6bb65bea4d501949c59c3a3
alvax2012 <39165799+alvax2012@users.noreply.github.com>
2024-03-06 15:26:14 +0800

    Update index.py
    1

--- index.py
+++ index.py
@@ -1,4 +1,14 @@
 print("hello!")
 
-for i in range(s)
-print (i)
+
+rangelist = range(10)
+[0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+for number in rangelist:
+    
+if number in (3, 4, 7, 9):
+    break
+else:
+    continue
+else:
+pass
