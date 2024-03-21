90adcd6e232c5c9050e30873f9a5d08f3a02aab3
alvax2012 <39165799+alvax2012@users.noreply.github.com>
2024-03-21 10:55:45 +0800

    Update index.py
    1

--- index.py
+++ index.py
@@ -1,6 +1,32 @@
-word = 'Hello, World'
-print("word1") # Hello, World1
-name = 'Вася'
-isUse=True
-if isUse:
-    print('Здесь был1', name)
+#word = 'Hello, World'
+#print("word1") # Hello, World1
+#name = 'Вася'
+#isUse=True
+#if isUse:
+#    print('Здесь был1', name)
+#
+
+#for i in range(1,3,2):
+#    print(i)
+count=0    
+w="qttwert!"
+for i in w:
+    if i=="t":
+        count+=1
+i=1        
+print("count:", count)
+while    i<=5:
+    #print(i)
+    i+=2
+    print(i)
+
+
+found= None
+for i in "hello":
+    if i=="e":
+        found=True
+        break
+    else:
+        found=False    
+        
+print(found)
