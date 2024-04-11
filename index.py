94ccac71b05c52803b0ed220485d04fe9a8eb526
alvax2012 <39165799+alvax2012@users.noreply.github.com>
2024-04-11 14:27:59 +0800

    1

--- index.py
+++ index.py
@@ -3,5 +3,12 @@
 #main222111
 #333
 #8888999
-country ={'33':'1','22':'2','33':'34'}
-print(country['33'])
+#country ={'33':'1','22':'2','33':'34'}
+#print(country['33'])
+
+#file=open('text.txt', 'w')
+#file.write('111111')
+#file.close()
+
+print('111333')
+print('22333')
